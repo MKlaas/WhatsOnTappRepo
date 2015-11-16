88bedc07c610a71ca5dcf6840b209743a7de2faa
Michael Klaas <klaas_mak@yahoo.com>
2015-11-16 14:16:55 -0500

    test
    testing git push

--- whatsontapp/site/about.php
+++ whatsontapp/site/about.php
@@ -49,7 +49,7 @@ include('navbar.php');
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Bar Page
+                <h1 class="page-header">Bar Page TESTING
                     <small>(bar name)</small>
                 </h1>
 				<!--
