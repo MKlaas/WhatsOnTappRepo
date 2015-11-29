75269d12541b6f520354cf1bbf84ef8e7e5f3f0f
slpeavy <slpeavym@gmail.com>
2015-11-28 22:40:24 -0500

    corrected issue

--- whatsontapp/site/index.php
+++ whatsontapp/site/index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<div style="height:45px; width: 100%;background-color: black;">
+    <?php
+    include('includes/navbar_profile.php'); 
+    ?>
+</div>
 
 
 <head>
